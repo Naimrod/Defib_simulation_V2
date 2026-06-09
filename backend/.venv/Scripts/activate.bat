d65e99b202114cb295ef4287516da78b9908db5b
AlexHam1702 <alexandre.hamard1702@gmail.com>
2026-06-09 10:58:22 +0200

    installed fastapi clean

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\light\dev\testfastapi\.venv
+set VIRTUAL_ENV=C:\Users\light\Documents\GitHub\Defib_simulation_V2\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
