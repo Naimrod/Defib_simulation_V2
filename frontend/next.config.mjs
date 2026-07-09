01cd8ae6d158a08732c54d1610ece95fdafcd9a0
mdinh <marc@naimrod.com>
2026-07-09 14:16:09 +0200

    uncomment static output build in config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: "export", 
+  output: "export", 
   // Optimisations de performance
   experimental: {
     optimizePackageImports: ['lucide-react', 'gsap'],
