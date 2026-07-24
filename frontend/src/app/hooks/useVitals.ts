162d2e49efaf7cc782afef7165899d841375dade
AlexHam1702 <alexandre.hamard1702@gmail.com>
2026-07-24 12:09:35 +0200

    removed useless PulselessRythms, thanks Raff

--- frontend/src/app/hooks/useVitals.ts
+++ frontend/src/app/hooks/useVitals.ts
@@ -284,7 +284,7 @@ export const useVitals = () => {
  useEffect(() => {
     const interval = setInterval(() => {
       setCosmeticVitals(prev => {
-        const pulselessRhythms = ["fibrillationVentriculaire", "asystole", "fv", "asysto", "arret","choc", "tachy_a", "tachycardieAtriale","tsv", "fib_a","fibrillationAtriale", "tv_1", "tachycardieVentriculaire","tv_2","tvType2"];
+        const pulselessRhythms = ["fibrillationVentriculaire", "asystole", "fv", "asysto", "arret","choc", "tv_1", "tachycardieVentriculaire","tv_2","tvType2"];
         const isPulsing = !pulselessRhythms.includes(vitals.rhythm) && vitals.bpm >0;
 
         const targetBpm = vitals.bpm;
@@ -370,7 +370,7 @@ export const useVitals = () => {
     window.location.href = "/connect";
   }, []);
 
-  const pulselessRhythms = ["fibrillationVentriculaire", "asystole", "fv", "asysto", "arret","choc", "tachy_a", "tachycardieAtriale","tsv", "fib_a","fibrillationAtriale", "tv_1", "tachycardieVentriculaire","tv_2","tvType2"];
+  const pulselessRhythms = ["fibrillationVentriculaire", "asystole", "fv", "asysto", "arret","choc", "tv_1", "tachycardieVentriculaire","tv_2","tvType2"];
   const hasPulse = !pulselessRhythms.includes(vitals.rhythm) && vitals.bpm > 0;
 
   let bpDisplay = "--/--";
